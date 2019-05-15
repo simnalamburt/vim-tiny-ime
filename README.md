dbf7b49e7e5fa9577100c2447a75a2566c848d4a
Hyeon Kim <simnalamburt@gmail.com>
2019-05-15 14:07:46 +0900

    Rename project: vim-osx-ime → vim-tiny-ime

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-vim-osx-ime
+vim-tiny-ime
 ========
+Tiny automatic IME switcher for macOS.
 
 -   Switch English IME whenever vim switchs to normal mode.
 -   When vim switches to insert mode:
@@ -7,3 +8,13 @@ vim-osx-ime
         IME and put it in Chinese mode.
     -   If the character before cursor is an English character, switch to Chinese
         IME and put it in English mode.
+
+<br>
+
+--------
+
+*vim-tiny-ime* is primarily distributed under the terms of the [GNU General
+Public License, version 3] or any later version. See [COPYRIGHT] for details.
+
+[GNU General Public License, version 3]: LICENSE
+[COPYRIGHT]: COPYRIGHT
--- input-method.m
+++ input-method.m
@@ -1,24 +1,3 @@
-// changeInput
-// Changes input language
-//
-// author:   Stefan Klieme (based on an idea by Craig Williams)
-// created:  2009-11-05
-// Usage:    changeInput                        prints current input language
-//           changeInput    name                changes input language to name
-
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 #import <Foundation/Foundation.h>
 #import <Carbon/Carbon.h>
 
