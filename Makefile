9d47ed7768c3b6c476dc4bd1972af3564f067209
Hyeon Kim <simnalamburt@gmail.com>
2019-05-15 17:16:38 +0900

    Replace forked set-ime.m with my own swift implementation

--- Makefile
+++ Makefile
@@ -1,3 +1,5 @@
-all: set-ime
-%: %.m
+all: set-ime.1 set-ime.2
+%.1: %.m
 	clang $< -o $@ -l objc -framework foundation -framework carbon -Os -Wall -Wextra -Wpedantic
+%.2: %.swift
+	swiftc $< -o $@ -O
---
+++ set-ime.swift
@@ -0,0 +1,40 @@
+import Carbon
+
+//
+// Read ARGV[1]
+//
+let argv = CommandLine.arguments
+if argv.count != 2 { exit(1) }
+let desired_ime_name = argv[1]
+
+//
+// Lookup all available IMEs and find desired IME
+//
+var chosen_ime: TISInputSource? = nil
+let ime_list = TISCreateInputSourceList(nil, false).takeRetainedValue() as! [TISInputSource]
+for ime in ime_list {
+  if let ime_name_ptr = TISGetInputSourceProperty(ime, kTISPropertyLocalizedName) {
+    let ime_name = Unmanaged<AnyObject>.fromOpaque(ime_name_ptr).takeUnretainedValue() as! String
+    if (desired_ime_name == ime_name) {
+      chosen_ime = ime
+      break
+    }
+  }
+}
+
+//
+// Check if the lookup was successful
+//
+if chosen_ime == nil {
+  print("Input source '\(desired_ime_name)' is not available")
+  exit(1)
+}
+
+//
+// Change IME
+//
+let err = TISSelectInputSource(chosen_ime)
+if err != 0 {
+  print("Could not change input language (OSStatus = \(err))")
+  exit(1)
+}
