a49c3072c88110ebf75698369e56cb6f459973ed
Hyeon Kim <simnalamburt@gmail.com>
2019-05-15 16:22:54 +0900

    Modernized imput-method.m

--- input-method.m
+++ input-method.m
@@ -1,50 +1,44 @@
 #import <Carbon/Carbon.h>
 
-//
-// Main logic
-//
-static int change_ime() {
-  // Read ARGV[1]
-  NSString *desired_ime_name = [[[NSProcessInfo processInfo] arguments] objectAtIndex:1];
+int main() {
+  @autoreleasepool {
+    //
+    // Read ARGV[1]
+    //
+    NSArray<NSString*> *argv = [[NSProcessInfo processInfo] arguments];
+    if ([argv count] != 2) { return 1; }
+    NSString *desired_ime_name = argv[1];
 
-  // Lookup all available IMEs and fined desired IME
-  TISInputSourceRef chosen_ime = nil;
-  NSArray *ime_list = (NSArray*)TISCreateInputSourceList(NULL, false);
-  for (NSUInteger i = 0; i < [ime_list count]; ++i) {
-    TISInputSourceRef ime = (TISInputSourceRef)[ime_list objectAtIndex:i];
-    NSString *ime_name = TISGetInputSourceProperty(ime, kTISPropertyLocalizedName);
-    if ([desired_ime_name isEqualToString:ime_name]) {
-      chosen_ime = ime;
-      break;
+    //
+    // Lookup all available IMEs and find desired IME
+    //
+    TISInputSourceRef chosen_ime = nil;
+    NSArray *ime_list = (NSArray*)TISCreateInputSourceList(nil, false);
+    for (id elem in ime_list) {
+      TISInputSourceRef ime = (TISInputSourceRef)elem;
+      NSString *ime_name = TISGetInputSourceProperty(ime, kTISPropertyLocalizedName);
+
+      if ([desired_ime_name isEqualToString:ime_name]) {
+        chosen_ime = ime;
+        break;
+      }
+    }
+
+    //
+    // Check if the lookup was successful
+    //
+    if (!chosen_ime) {
+      fprintf(stderr, "Input source '%s' is not available\n", [desired_ime_name UTF8String]);
+      return 1;
+    }
+
+    //
+    // Change IME
+    //
+    const OSStatus err = TISSelectInputSource(chosen_ime);
+    if (err) {
+      fprintf(stderr, "Could not change input language (OSStatus = %i)\n", (int)err);
+      return 1;
     }
   }
-  [ime_list release];
-
-  // Find desired IME
-  if (!chosen_ime) {
-    fprintf(stderr, "Input source '%s' is not available\n", [desired_ime_name UTF8String]);
-    return 1;
-  }
-
-  // Change IME
-  const OSStatus err = TISSelectInputSource(chosen_ime);
-  if (err) {
-    fprintf(stderr, "Could not change input language (OSStatus = %i)\n", (int)err);
-    return 1;
-  }
-
-  return 0;
-}
-
-//
-// Entry point
-//
-int main(int argc, const char __attribute__((unused)) *argv[]) {
-  if (argc < 2) { return 1; }
-
-  NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
-  const int ret = change_ime();
-  [pool release];
-
-  return ret;
 }
