a531ce4a288ed4fba62bb3dcfc05e80f5d1b0443
Hyeon Kim <simnalamburt@gmail.com>
2019-05-15 14:17:29 +0900

    Simplify input-method.m
    1.  Delete unused codes
    2.  Enhance comments

--- input-method.m
+++ input-method.m
@@ -1,39 +1,50 @@
-#import <Foundation/Foundation.h>
 #import <Carbon/Carbon.h>
 
-int main(int argc, const char* __attribute__((unused)) argv[]) {
-  NSAutoreleasePool* pool = [[NSAutoreleasePool alloc] init];
+//
+// Main logic
+//
+static int change_ime() {
+  // Lookup all available IMEs
+  NSArray *inputArray = (NSArray*)TISCreateInputSourceList(NULL, false);
+  NSMutableDictionary *availableLanguages = [NSMutableDictionary dictionaryWithCapacity:[inputArray count]];
+  for (NSUInteger i = 0; i < [inputArray count]; ++i) {
+    NSString *name = TISGetInputSourceProperty(
+        (TISInputSourceRef)[inputArray objectAtIndex:i],
+        kTISPropertyLocalizedName);
 
-  if (argc == 1) {
-    TISInputSourceRef current = TISCopyCurrentKeyboardInputSource();
-    NSString* currentName = TISGetInputSourceProperty(current, kTISPropertyLocalizedName);
-    printf("%s\n", [currentName UTF8String]);
-    CFRelease(current);
-  } else {
-    NSArray* inputArray = (NSArray*)TISCreateInputSourceList(NULL, false);
-    NSMutableDictionary* availableLanguages = [NSMutableDictionary dictionaryWithCapacity:[inputArray count]];
-    for (NSUInteger i = 0; i < [inputArray count]; ++i) {
-      const void *name = TISGetInputSourceProperty(
-          (TISInputSourceRef)[inputArray objectAtIndex:i],
-          kTISPropertyLocalizedName);
+    [availableLanguages setObject:[inputArray objectAtIndex:i] forKey:name];
+  }
+  [inputArray release];
 
-      [availableLanguages setObject:[inputArray objectAtIndex:i] forKey:name];
-    }
-    [inputArray release];
+  // Read ARGV[1]
+  NSString *chosenInput = [[[NSProcessInfo processInfo] arguments] objectAtIndex:1];
 
-    NSString *chosenInput = [[[NSProcessInfo processInfo] arguments] objectAtIndex:1];
-    TISInputSourceRef chosen = (TISInputSourceRef)[availableLanguages objectForKey:chosenInput];
-    if (chosen) {
-      OSStatus err = TISSelectInputSource(chosen);
-      if (err) {
-        printf("Could not change input language (error %i)\n", (int)err);
-      } else {
-        printf("Changed input language to %s\n", [chosenInput UTF8String]);
-      }
-    } else {
-      printf("%s not available\n", [chosenInput UTF8String]);
-    }
+  // Find desired IME
+  TISInputSourceRef chosen = (TISInputSourceRef)[availableLanguages objectForKey:chosenInput];
+  if (!chosen) {
+    fprintf(stderr, "Input source '%s' is not available\n", [chosenInput UTF8String]);
+    return 1;
   }
 
+  // Change IME
+  const OSStatus err = TISSelectInputSource(chosen);
+  if (err) {
+    fprintf(stderr, "Could not change input language (OSStatus = %i)\n", (int)err);
+    return 1;
+  }
+
+  return 0;
+}
+
+//
+// Entry point
+//
+int main(int argc, const char __attribute__((unused)) *argv[]) {
+  if (argc < 2) { return 1; }
+
+  NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+  const int ret = change_ime();
   [pool release];
+
+  return ret;
 }
